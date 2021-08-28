a85da66c9d0b81b4eb1e00c61e8d12bffb89ff28
deathbybandaid <github@deathbybandaid.net>
2021-08-28 11:12:38 -0400

    Repair Plugin

--- web/webwatch_api.py
+++ web/webwatch_api.py
@@ -17,6 +17,12 @@ class WebWatch_Tuner():
 
     def get(self, *args):
 
+        stream_method = "webwatch"
+
+        base_url = request.url_root[:-1]
+
+        tuner_number = request.args.get('tuner', default=None, type=str)
+
         client_address = request.remote_addr
 
         accessed_url = request.args.get('accessed', default=request.url, type=str)
@@ -40,8 +46,18 @@ class WebWatch_Tuner():
 
                 if str(channel_number) in [str(x) for x in self.fhdhr.device.channels.get_channel_list("number", origin)]:
                     chan_obj = self.fhdhr.device.channels.get_channel_obj("number", channel_number, origin)
+                    if not chan_obj:
+                        response = Response("Not Found", status=404)
+                        response.headers["X-fHDHR-Error"] = "801 - Unknown Channel"
+                        self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
+                        abort(response)
                 elif str(channel_number) in [str(x) for x in self.fhdhr.device.channels.get_channel_list("id", origin)]:
                     chan_obj = self.fhdhr.device.channels.get_channel_obj("id", channel_number, origin)
+                    if not chan_obj:
+                        response = Response("Not Found", status=404)
+                        response.headers["X-fHDHR-Error"] = "801 - Unknown Channel"
+                        self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
+                        abort(response)
                 else:
                     response = Response("Not Found", status=404)
                     response.headers["X-fHDHR-Error"] = "801 - Unknown Channel"
@@ -52,6 +68,11 @@ class WebWatch_Tuner():
 
                 if str(channel_number) in [str(x) for x in self.fhdhr.device.channels.get_channel_list("id")]:
                     chan_obj = self.fhdhr.device.channels.get_channel_obj("id", channel_number)
+                    if not chan_obj:
+                        response = Response("Not Found", status=404)
+                        response.headers["X-fHDHR-Error"] = "801 - Unknown Channel"
+                        self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
+                        abort(response)
                 else:
                     response = Response("Not Found", status=404)
                     response.headers["X-fHDHR-Error"] = "801 - Unknown Channel"
@@ -69,20 +90,45 @@ class WebWatch_Tuner():
 
             duration = request.args.get('duration', default=0, type=int)
 
+            transcode_quality = request.args.get('transcode', default=None, type=str)
+            valid_transcode_types = [None, "heavy", "mobile", "internet720", "internet480", "internet360", "internet240"]
+            if transcode_quality not in valid_transcode_types:
+                response = Response("Service Unavailable", status=503)
+                response.headers["X-fHDHR-Error"] = "802 - Unknown Transcode Profile"
+                self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
+                abort(response)
+
             stream_args = {
                             "channel": channel_number,
                             "origin": origin,
-                            "method": "webwatch",
+                            "method": stream_method,
                             "duration": duration,
-                            "origin_quality": self.fhdhr.config.dict["webwatch"]["origin_quality"],
-                            "transcode_quality": None,
+                            "origin_quality": self.fhdhr.config.dict["streaming"]["origin_quality"],
+                            "transcode_quality": transcode_quality or self.fhdhr.config.dict["streaming"]["transcode_quality"],
                             "accessed": accessed_url,
+                            "base_url": base_url,
                             "client": client_address,
                             "client_id": session["session_id"]
                             }
 
+            if stream_method == "passthrough":
+                try:
+                    stream_args = self.fhdhr.device.tuners.get_stream_info(stream_args)
+                except TunerError as e:
+                    self.fhdhr.logger.info("A %s stream request for %s channel %s was rejected due to %s"
+                                           % (origin, stream_args["method"], str(stream_args["channel"]), str(e)))
+                    response = Response("Service Unavailable", status=503)
+                    response.headers["X-fHDHR-Error"] = str(e)
+                    self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
+                    abort(response)
+                self.fhdhr.logger.info("Passthrough method selected, no tuner will be used. Redirecting Client to %s" % stream_args["stream_info"]["url"])
+                return redirect(stream_args["stream_info"]["url"])
+
             try:
-                tunernum = self.fhdhr.device.tuners.first_available(origin, channel_number)
+                if not tuner_number:
+                    tunernum = self.fhdhr.device.tuners.first_available(origin, channel_number)
+                else:
+                    tunernum = self.fhdhr.device.tuners.tuner_grab(tuner_number, origin, channel_number)
             except TunerError as e:
                 self.fhdhr.logger.info("A %s stream request for channel %s was rejected due to %s"
                                        % (stream_args["method"], str(stream_args["channel"]), str(e)))
@@ -109,14 +155,15 @@ class WebWatch_Tuner():
             session["tuner_used"] = tunernum
 
             try:
-                stream = tuner.get_stream(stream_args, tuner)
+                tuner.setup_stream(stream_args, tuner)
             except TunerError as e:
+                response = Response("Service Unavailable", status=503)
                 response.headers["X-fHDHR-Error"] = str(e)
                 self.fhdhr.logger.error(response.headers["X-fHDHR-Error"])
                 tuner.close()
                 abort(response)
 
-            return Response(stream.get())
+            return Response(tuner.stream.get())
 
         if redirect_url:
             return redirect("%s?retmessage=%s" % (redirect_url, urllib.parse.quote("%s Success" % method)))
